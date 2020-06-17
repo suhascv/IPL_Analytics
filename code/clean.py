8d6ea8867fd8a23dbabfb9a84ef1de9cde6fac6c
Suhas CV <suhazcsk@gmail.com>
2020-06-17 01:57:01 -0400

    players collection created

--- code/clean.py
+++ code/clean.py
@@ -1,3 +1,48 @@
 import yaml
-file=open('.yaml')
-yaml.load(file, Loader=yaml.FullLoader)
+import glob
+import pymongo
+
+
+    
+def main():
+    myClient=pymongo.MongoClient("mongodb://localhost:27017")
+    
+
+    all_files=[]
+    for file in glob.glob("../Data/ipl/*.yaml"):
+        #looping through all files in directory
+        file_name=file.strip('../Data/ipl/').strip('.yaml')
+        all_files.append(int(file_name))
+    
+    #sort_files by name
+    all_files.sort()
+    
+    players=set()
+    errors=[]
+    for f in all_files:
+        file=open("../Data/ipl/"+str(f)+'.yaml')
+        data=yaml.load(file, Loader=yaml.FullLoader)
+        try:
+            for i in range(2):
+                if i==0:
+                    innings=data['innings'][0]['1st innings']
+                else:
+                    innings=data['innings'][1]['2nd innings']
+                deleveries=innings['deliveries']
+                for d in deleveries:
+                    v=list(d.values())
+                    players.add(v[0]['batsman'])
+                    players.add(v[0]['bowler'])
+        except:
+            errors.append(f)
+    
+    print(players)
+    print(len(players))
+    
+    
+
+
+
+
+if __name__=='__main__':
+    main()
---
+++ code/load_player.py
@@ -0,0 +1,71 @@
+import pymongo
+from datetime import date
+"""
+creating players collection
+"""
+
+def calculateAge(dob):
+    """
+    calculating age from dob
+    """
+    dob=dob.split('-')
+    born=date(int(dob[0]),int(dob[1]),int(dob[2]))
+    today=date.today()
+    try:
+        birthday=born.replace(year=today.year)
+
+    except:
+        birthday = born.replace(month=born.month+1,year=today.year,day=1)
+    
+    if birthday > today:
+        return today.year-born.year-1
+    else:
+        return today.year-born.year
+
+
+
+
+def main():
+    path='../Data/csv-files/'
+    data=open(path+'Player.csv')
+    batting = open(path+'Batting_Style.csv')
+    bowling= open(path+'Bowling_Style.csv')
+
+    batting_skill={}
+    bowling_skill={}
+    batting.readline()
+    bowling.readline()
+    for style in batting:
+        s=style.strip().split(',')
+        batting_skill[s[0]]=s[1]
+
+    for style in bowling:
+        s=style.strip().split(',')
+        bowling_skill[s[0]]=s[1]
+
+
+    players=[]
+    data.readline()
+    for player in data:
+        atts=player.strip().split(',')
+        
+        player_dict={
+            '_id':atts[0],
+            'name':atts[1],
+            'age':calculateAge(atts[2])       
+            }
+        if atts[3]!='':
+            player_dict['batting_hand']=batting_skill[atts[3]]
+        if atts[4]!='':    
+            player_dict['bowling_skill']=bowling_skill[atts[4]] 
+        players.append(player_dict)
+    
+    myClient=pymongo.MongoClient('mongodb://localhost:27017')
+    myDb=myClient['IPL']
+    myCol=myDb['players']
+
+    myCol.insert_many(players)
+
+    
+if __name__ =='__main__':
+    main()
