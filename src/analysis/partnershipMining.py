c3731afd99c4120cffe53aff9644c089f3c29968
Suhas CV <suhazcsk@gmail.com>
2020-07-02 22:36:20 -0400

    l2 loaded most frequent partnerships mined

--- src/analysis/partnershipMining.py
+++ src/analysis/partnershipMining.py
@@ -9,10 +9,11 @@ def main():
     myDb     = myClient['IPL']
     print('db connected')
 
-
+    min_runs=30
+    min_suppport=10
     #Creating L1 of item set mining
     Parnterships = myDb['partnerships']
-    partnerships = list(Parnterships.find({'total_runs':{'$gte':20}}))
+    partnerships = list(Parnterships.find({'total_runs':{'$gte':min_runs}}))
 
     
     l1={}
@@ -29,21 +30,51 @@ def main():
     #L1 connection
     L1 = myDb['L1']  
     lcnt=0
+    #L1 pruning and load to db
     for item in l1:
         #min support=15
-        if l1[item]>15:
-            L1.insert_one({'player':item,'count':l1[item]})
+        if l1[item]>min_suppport:
+            L1.insert_one({'_id':item,'count':l1[item]})
             lcnt+=1
     
     print('L1 loaded to database, ',lcnt,' items in L1')
-
-    #pruning L2 candidates(already in partnerships collection) using L1
+    """
+    generating l2 candidates from l1 is not required since the pruned version is present in partnerships collection
+    further pruning L2 candidates(already in partnerships collection) using L1
+    """
+    l2={}
     for p in partnerships:
-        pass
+        partners=[]
+        partners.append(p['partner1'])
+        partners.append(p['partner2'])
+        partners.sort()
+        
+        if partners[0] in l1 and partners[1] in l1:
+            pkey='-'.join(partners)
+            try:
+                l2[pkey]+=1
+            except:
+                l2[pkey]=1
+    
 
+    #L2 connection
+    L2 = myDb['L2']
+    l2cnt=0
+    #L1 pruning and load to db
+    for item in l2:
+        #min support=15
+        if l2[item]>min_suppport:
+            L2.insert_one({'_id':item,'count':l2[item]})
+            l2cnt+=1
 
+    print('L2 loaded to database, ',l2cnt,' items in L2')
 
-
+    print('most frequent partnerships with total_runs > ',min_runs)
+    most_frequent_partnerships=list(L2.aggregate([{'$sort':{'count':-1}}]))
+    
+    for fitem in most_frequent_partnerships:
+        print(fitem['_id'],' : ',fitem['count'])
+        
 
 
 if __name__=='__main__':
